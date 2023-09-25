5f1c9ed456cd3d1e8c8bee3b0352c47e65c7fe5e
Crowmium1 <l.jfitzgerald@hotmail.com>
2023-09-25 02:53:11 +0100

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,17 @@
-#cal
-
 """
-Calculator library containing basic math operations.
+Unit tests for the calculator library
 """
 
-def add(first_term, second_term):
-    return first_term + second_term
+import calculator
 
-def subtract(first_term, second_term):
-    return first_term - second_term
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
+
+def test_multiplication(self):
+    assert 100 == calculator.multiply(10, 10)
